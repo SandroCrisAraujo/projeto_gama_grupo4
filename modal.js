c7dca851d1cc786fbeee64d8ab057ef98f7c37ef
sandro <sandro.c.p.a@hotmail.com>
2023-05-22 18:17:51 -0300

    Ajustando responsividade

--- modal.js
+++ modal.js
@@ -1,12 +1,29 @@
 // Recebe o modal
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+
 // Recebe o botão que abre o modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+
 // <span> para fechar o modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
 // block modal
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+  modal3.style.display = "block";
 }
 
 // Quando clicar (x), fecha modal
@@ -14,9 +31,29 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
 // Quando clicar fora do modal, ele fecha
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
   }
 }
